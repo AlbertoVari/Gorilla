34fcca4913dac5e079915587f03ff071857dee2c
Alberto Varignana <22276126+AlbertoVari@users.noreply.github.com>
2021-03-08 12:49:48 +0100

    Add files via upload

---
+++ left-arm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+
+# TODO: Add code here
+
+ts = TouchSensor()
+leds = Leds()
+
+leds.set_color("LEFT", "GREEN")
+leds.set_color("RIGHT", "GREEN")
+sound = Sound()
+sound.speak('Gorilla move left arm')
+
+m = LargeMotor(OUTPUT_A)
+m.on_for_rotations(SpeedPercent(10),0.2)
+m.on_for_rotations(SpeedPercent(-10),0.2)
+
+
+leds.set_color("LEFT", "RED")
+leds.set_color("RIGHT", "RED")
+
+sleep(0.01)
